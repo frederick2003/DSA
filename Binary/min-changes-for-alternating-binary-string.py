9641e48ab89bd81a46aa2143b8e3951846ebfdde
frederick2003 <fredhamilton2003@gmail.com>
2026-03-05 20:11:04 +0000

    Added leetcode daily exercise: min changes to make alternating binary string

---
+++ Binary/min-changes-for-alternating-binary-string.py
@@ -0,0 +1,20 @@
+def minOperations(self, s):
+    if len(s) ==  1:
+        return 0
+    
+    one_first_count = 0
+    zero_first_count = 0
+
+    for i in range(len(s)):
+        if i%2 == 0:
+            if s[i] != "1":
+                one_first_count += 1
+            if s[i] != "0":
+                zero_first_count += 1 
+        else:
+            if s[i] != "1":
+                zero_first_count += 1
+            if s[i] != "0":
+                one_first_count += 1
+
+    return min(one_first_count, zero_first_count)
