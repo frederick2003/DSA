6f0f48a77cdf24912c150f8dc0a8e4ad78bb1157
frederick2003 <fredhamilton2003@gmail.com>
2026-03-08 17:13:30 +0000

    Adding more 2Pointer exercises

---
+++ 2-Pointers/Valid-Triangle-Number.py
@@ -0,0 +1,15 @@
+def triangleNumber(nums):
+    count = 0
+    nums.sort()
+    for i in range(len(nums) - 1, -1, -1):
+        left = 0
+        right = i - 1
+        while left < right:
+            if nums[left] + nums[right] > nums[i]:
+                count += right - left
+                right -= 1
+            else:
+                left += 1
+    return count
+        
+print(triangleNumber([4,2,3,4]))
